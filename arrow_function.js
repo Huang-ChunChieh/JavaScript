4509acb6e2a24dd7906d5b39bf2e1742b8574762
Huang-ChunChieh <jay24585978@gmail.com>
2022-11-10 19:32:27 +0800

    add arrow_function example

---
+++ arrow_function.js
@@ -0,0 +1,16 @@
+var arr = [1,2,3,4,5]
+console.log(
+    arr.filter(function(value){
+        return value > 1
+    })
+    .map(function(value){
+        return value * 2
+    })
+)
+
+// Use Arrow Function
+console.log(
+    arr
+    .filter(value => value > 1) 
+    .map(value => value * 2)
+  )
